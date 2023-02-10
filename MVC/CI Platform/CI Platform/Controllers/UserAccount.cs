6e93af0cbd8a2c67a29662b50458ef1bbf173145
HarshRathod <harshrathod982002@gmail.com>
2023-02-10 17:08:09 +0530

    MVC Folder created

---
+++ MVC/CI Platform/CI Platform/Controllers/UserAccount.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_Platform.Controllers
+{
+    public class UserAccount : Controller
+    {
+        public IActionResult login()
+        {
+            return View();
+        }
+        
+    }
+}
