76aa2ffa433a3c9b4984a072d59cc960133a8593
HarshRathod <harshrathod982002@gmail.com>
2023-02-13 11:39:29 +0530

    Login page using MVC

---
+++ MVC/CI PLATFORM/CI PLATFORM/Controllers/UserAccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_PLATFORM.Controllers
+{
+    public class UserAccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
